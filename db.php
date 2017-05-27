698302f4db989604837579064bd7a8724ff26dcb
Sathriyansiva <sathriyan8@gmail.com>
2017-05-27 11:22:17 +0530

    Update db.php

--- db.php
+++ db.php
@@ -17,7 +17,7 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-//else{
-//echo ("sucess");
-//}
+else{
+echo ("sucess");
+}
 ?>
